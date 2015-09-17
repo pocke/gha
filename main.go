c9984f33dcb10991b820d9f206e7b584c3c2f06c
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-09-17 21:49:43 +0900

    Enable to work when can't use Stdin

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"syscall"
 
-	"github.com/howeyc/gopass"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 type token struct {
@@ -31,15 +32,10 @@ func CLI(appName, fname string) (string, error) {
 		return string(b), err
 	}
 
-	fmt.Print("username: ")
-
-	sc := bufio.NewScanner(os.Stdin)
-	sc.Split(bufio.ScanLines)
-	sc.Scan()
-	user := sc.Text()
-
-	fmt.Printf("password for %s (never stored): ", user)
-	pass := string(gopass.GetPasswd())
+	user, pass, err := GetUserInfo()
+	if err != nil {
+		return "", err
+	}
 
 	key, err := Auth(user, pass, appName)
 	if err != nil {
@@ -50,6 +46,85 @@ func CLI(appName, fname string) (string, error) {
 		return "", err
 	}
 	return key, nil
+}
+
+func GetUserInfo() (string, string, error) {
+	user, err := ReadUsername()
+	if err != nil {
+		return "", "", err
+	}
+
+	pass, err := ReadPassword(user)
+	if err != nil {
+		return "", "", err
+	}
+
+	return user, pass, nil
+}
+
+func ReadUsername() (string, error) {
+	tty, err := os.Open("/dev/tty")
+	if err != nil {
+		return "", err
+	}
+	defer tty.Close()
+
+	fmt.Print("username: ")
+	sc := bufio.NewScanner(tty)
+	sc.Split(bufio.ScanLines)
+	sc.Scan()
+	return sc.Text(), nil
+}
+
+func ReadPassword(user string) (string, error) {
+	fmt.Printf("password for %s (never stored): ", user)
+	res := make([]byte, 0)
+
+	for {
+		v, err := ReadCharAsPassword()
+		if err != nil {
+			return "", err
+		}
+
+		if (v == 127 || v == 8) && len(res) > 0 {
+			res = res[:len(res)-1]
+			os.Stdout.Write([]byte("\b \b"))
+		}
+
+		if v == 13 || v == 10 {
+			return string(res), nil
+		}
+
+		// C-c or C-d
+		if v == 3 || v == 4 {
+			return "", fmt.Errorf("Exited by user")
+		}
+
+		if v != 0 {
+			res = append(res, v)
+		}
+	}
+}
+
+func ReadCharAsPassword() (byte, error) {
+	tty, err := os.Open("/dev/tty")
+	if err != nil {
+		return 0, nil
+	}
+	defer tty.Close()
+	fd := int(tty.Fd())
+
+	if oldState, err := terminal.MakeRaw(fd); err != nil {
+		return 0, nil
+	} else {
+		defer terminal.Restore(fd, oldState)
+	}
+
+	var buf [1]byte
+	if n, err := syscall.Read(fd, buf[:]); n == 0 || err != nil {
+		return 0, err
+	}
+	return buf[0], nil
 }
 
 // Auth gets Psersonal access token of GitHub.
